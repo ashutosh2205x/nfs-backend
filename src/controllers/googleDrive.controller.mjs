9d663e884b6833992abaedd078739e8642e5bfcb
Ashutosh Bharti <ashutosh.bharti5247@gmail.com>
2022-12-18 10:02:13 +0530

    g-drive apis configured

---
+++ src/controllers/googleDrive.controller.mjs
@@ -0,0 +1,47 @@
+import * as path from "path";
+import * as fs from "fs";
+import { GoogleDriveService } from "../services/googleDrive.service";
+
+const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || "";
+const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || "";
+const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || "";
+const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || "";
+
+(async () => {
+  const googleDriveService = new GoogleDriveService(
+    driveClientId,
+    driveClientSecret,
+    driveRedirectUri,
+    driveRefreshToken
+  );
+
+  const finalPath = path.resolve(__dirname, "../tmp/file.xlsx");
+  const folderName = "Picture";
+  console.log("finalPath", finalPath);
+
+  if (!fs.existsSync(finalPath)) {
+    throw new Error("File not found!");
+  }
+
+  let folder = await googleDriveService
+    .searchFolder(folderName)
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+
+  if (!folder) {
+    folder = await googleDriveService.createFolder(folderName);
+  }
+
+  await googleDriveService
+    .saveFile("SpaceX", finalPath, "image/jpg", folder.id)
+    .catch((error) => {
+      console.error(error);
+    });
+
+  console.info("File uploaded successfully!");
+
+  // Delete the file on the server
+  fs.unlinkSync(finalPath);
+})();
